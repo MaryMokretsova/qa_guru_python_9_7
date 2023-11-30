0038c9e3e8ddeeacc3770aacb6eceb28802ae749
Mary <cameron105@mail.ru>
2023-11-30 23:17:30 +0300

    file

---
+++ script_pdf.py
@@ -0,0 +1,14 @@
+import os.path
+
+from pypdf import PdfReader
+
+reader = PdfReader("tmp/test_design.pdf")
+
+# print(reader.pages)
+# print(len(reader.pages))
+#
+# print(reader.pages[0].extract_text())
+
+# assert "Test Design" in reader.pages[0]
+
+printos.path.getsize("tmp/test_design.pdf")
--- test_download_file.py
+++ test_download_file.py
@@ -12,32 +12,32 @@ from script_os import TMP_DIR
 
 
 def test_text_in_dowloaded_file():
-options = webdriver.ChromeOptions()
+    options = webdriver.ChromeOptions()
 
-prefs = {
+    prefs = {
     "download.default_directory": TMP_DIR,
     "download.prompt_for_download": False
-}
-options.add_experimental_option("prefs", prefs)
+    }
+    options.add_experimental_option("prefs", prefs)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-browser.config.driver = driver
+    browser.config.driver = driver
 
-browser.open("https://github.com/pytest-dev/pytest/blob/main/README.rst")
-download_url = browser.element("[data-testid='raw-button']").get(query.attribute("href"))
-print(download_url)
+    browser.open("https://github.com/pytest-dev/pytest/blob/main/README.rst")
+    download_url = browser.element("[data-testid='raw-button']").get(query.attribute("href"))
+    print(download_url)
 
 
 
-content = requests.get(url="https://github.com/pytest-dev/pytest/blob/main/README.rst").content
+    content = requests.get(url="https://github.com/pytest-dev/pytest/blob/main/README.rst").content
 
-with open("tmp/readme2.rst", 'wb') as download_file:
-    download_file.write(content)
+    with open("tmp/readme2.rst", 'wb') as download_file:
+        download_file.write(content)
 
     with open(os.path.join(TMP_DIR, "readme2.rst"), 'wb') as download_file:
         file_content_str = download_file.read()
         assert "test_answer" in file_content_str
-# browser.element("[data-testid='download-raw-button']").click()
-# time.sleep(5)
+    # browser.element("[data-testid='download-raw-button']").click()
+    # time.sleep(5)
 
